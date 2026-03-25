4dec5f238d4f4e99c544fda8324b1fd377862e54
João Paulo Mota <99102663+motajoao1@users.noreply.github.com>
2026-03-25 15:33:15 -0300

    Add files via upload

---
+++ devs/ExemClienteApp.java
@@ -0,0 +1,6 @@
+public class ExemClienteApp {
+    public static void main (String[] args) {
+        ExemClientePf pf = new ExemClientePf(1, "Joao", "Vale", "123.456.789-01");
+        pf.exibirInfo();
+    }
+}
---
+++ devs/ExemClientePj.java
@@ -0,0 +1,19 @@
+public class ExemClientePj extends ExemCliente{
+    private String cnpj;
+
+    public ExemClientePj (int codigo, String nome, String endereco, String cnpj) {
+        super(codigo, nome,endereco);
+        this.cnpj=cnpj;
+    }
+    public String getCnpj (){
+        return cnpj;
+    }
+    public void getCnpj (String cnpj){
+        this.cnpj=cnpj;
+    }
+    public void exibirInfo(){
+            super.exibirInfo();
+        System.out.println("O cnpj é: " + getCnpj());
+        }
+    }
+
