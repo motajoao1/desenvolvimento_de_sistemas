82a679add13043d98ca1480a6efe16fc74189a8f
João Paulo Mota <99102663+motajoao1@users.noreply.github.com>
2026-05-04 16:49:24 -0300

    Add files via upload
    FEITO NO APACHE-NETBEANS

---
+++ telas/TELAS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.telas;
+
+/**
+ *
+ * @author aluno.den
+ */
+public class TELAS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
